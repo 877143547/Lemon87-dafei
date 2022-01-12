c554b75e1c553d37dafb4dbdcde197ba791d01ee
877143547 <877143547@qq.com>
2022-01-12 16:56:25 +0800

    1

---
+++ lesson1.py
@@ -0,0 +1,66 @@
+# Author : 柠檬班-亚萌
+# Project : lemon87
+# Time : 2021/12/24 20:42
+# E-mail : 3343787213@qq.com
+# Company : 湖南零檬信息技术有限公司
+# Site : http://www.lemonban.com
+# Forum : http://testingpai.com
+
+# print("hello world!!")
+# print("123123")
+# print("yameng")
+# print("亚萌是最帅的！！")
+
+# 注释，注释掉的内容是不会被执行的
+# 单行注释
+
+'''
+这是多行注释
+在这3个引号内，所有的内容都会被注释掉
+'''
+"""
+asdasdasdasd
+"""
+
+# python是要顶格写
+# 如果有子代码是需要缩进写的
+print("yameng")
+
+'''
+python的规范：
+标识符的规范：（变量名，函数名。。。自己定义名字的）
+1、只能包含数字、字母、下划线
+2、不能以数字开头  -- 基本也都是字母开头
+3、不能用关键字命名
+'False', 'None', 'True', 'and', 'as', 'assert', 'async', 
+'await', 'break', 'class', 'continue', 'def', 'del', 'elif',
+ 'else', 'except', 'finally', 'for', 'from', 'global', 
+ 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not',
+  'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 
+  'yield'
+4、不能用内置函数命名
+print,input,type,len.....
+5、为了方便阅读，英文翻译命名，太长建议下划线分隔(community_id)
+或者使用驼峰命名(communityId)
+
+代码规范：
+PEB8
+python之禅
+'''
+
+
+import keyword
+print(keyword.kwlist)
+
+
+
+
+
+
+
+
+
+
+
+
+
